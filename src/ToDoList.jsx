9cd793e1bca389d2b8e5e0142be4a10ee822e8f9
Emmar Caber <caberemmar@gmail.com>
2024-01-09 20:14:14 +0800

    feat: add some styles

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -6,16 +6,17 @@ function ToDoList() {
 
     return (
         <div className="container">
-            <p>
-                <input type="text" id="taskInput" placeholder="Add task" />
-                <button>Add</button>
-            </p>
+            <h1 className="title">To-Do-List</h1>
+            <div class="form-control">
+                <input type="text" id="taskInput" placeholder="Add task" className="form-input" />
+                <button class="btn btn-add">Add</button>
+            </div>
 
-            <ul>
+            <ol className="ordered-list">
                 {
                     tasks.map((task, index) => {
                         return (
-                            <li>
+                            <li className="list-items">
                                 <span className="txt-task">{task}</span>
                                 <button className="btn btn-delete">Delete</button>
                                 <button className="btn btn-up">Up</button>
@@ -24,7 +25,7 @@ function ToDoList() {
                         );
                     })
                 }
-            </ul>
+            </ol>
         </div>
     )
 }
