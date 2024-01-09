09d2fc095602a9f93bd77cf4d79cb7791734fb68
Emmar Caber <caberemmar@gmail.com>
2024-01-09 20:28:23 +0800

    feat: create addTask and delete task function

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -8,10 +8,23 @@ function ToDoList() {
     setNewTask(() => e.target.value);
   }
 
+  function addTask() {
+    // Add new tasks to array of tasks
+    setTasks((t) => [...t, newTask]);
+
+    // Empty the value of task-input
+    document.getElementById("taskInput").value = "";
+  }
+
+  function deleteTask(index) {
+    const filterTaskFunction = tasks.filter((_, i) => i !== index);
+    setTasks(filterTaskFunction);
+  }
+
   return (
     <div className="container">
       <h1 className="title">To-Do-List</h1>
-      <div class="form-control">
+      <div className="form-control">
         <input
           type="text"
           id="taskInput"
@@ -19,15 +32,22 @@ function ToDoList() {
           className="form-input"
           onChange={(e) => handleTaskInputChange(e)}
         />
-        <button class="btn btn-add">Add</button>
+        <button className="btn btn-add" onClick={addTask}>
+          Add
+        </button>
       </div>
 
       <ol className="ordered-list">
         {tasks.map((task, index) => {
           return (
-            <li className="list-items">
+            <li className="list-items" key={index}>
               <span className="txt-task">{task}</span>
-              <button className="btn btn-delete">Delete</button>
+              <button
+                className="btn btn-delete"
+                onClick={() => deleteTask(index)}
+              >
+                Delete
+              </button>
               <button className="btn btn-up">Up</button>
               <button className="btn btn-down">Down</button>
             </li>
